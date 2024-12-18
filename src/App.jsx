eadc6a8b52e7791c93633ebace422f82ee1e4798
pquauke <philipp.quauke@gmail.com>
2024-12-18 17:39:54 +0100

    Add initial files

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  return <></>
+}
+
+// Comp: nav bar
+//  Comp: cart icon with number that increases
+//  page: home
+//  page: shop
+//    Comp: add to cart button
+//    Comp: buy now button >> jumps to checkout directly
+//    Comp: product cards
+//  page: checkout
+//    Comp: increase / decrease amount buttons
+//    Comp: product cards
+//    Comp: order summary
+//    Comp: checkout button
+//  utils: https://fakestoreapi.com/
+
+// use prop names
+// use tests
+// style with css modules and icon components
