67f263dc4e432bb333ac5618c49bbe2c0fb0e7e5
pquauke <philipp.quauke@gmail.com>
2024-12-18 18:36:59 +0100

    Create react router structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 // import { useState } from 'react'
+import { Link, Outlet } from 'react-router-dom'
 import './App.css'
 
-export default function App() {
-  return <></>
+function App() {
+  return (
+    <>
+      <nav>
+        <Link to="homepage">homepage</Link>
+        <Link to="shop">shop</Link>
+        <Link to="checkout">checkout</Link>
+      </nav>
+      <Outlet />
+    </>
+  )
 }
-
+export default App
 // Comp: nav bar
 //  Comp: cart icon with number that increases
 //  page: home
 //  page: shop
-//    Comp: add to cart button
-//    Comp: buy now button >> jumps to checkout directly
-//    Comp: product cards
 //  page: checkout
-//    Comp: increase / decrease amount buttons
-//    Comp: product cards
-//    Comp: order summary
-//    Comp: checkout button
 //  utils: https://fakestoreapi.com/
 
 // use prop names
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import routes from './routes'
 import './index.css'
-import App from './App.jsx'
+
+const router = createBrowserRouter(routes)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    {/* <App /> */}
+    <RouterProvider router={router} />
+  </StrictMode>
 )
